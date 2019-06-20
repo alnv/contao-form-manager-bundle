17c65ecbeaf53c6132d632663024fd9af0ae7eb2
Alexander Naumov <me@alexandernaumov.de>
2019-06-20 11:58:30 +0200

    rm vue

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,4 @@
 <?php
 
-$objAssetsManager = \Alnv\ContaoAssetsManagerBundle\Library\AssetsManager::getInstance();
-$objAssetsManager->addIfNotExist( 'bundles/alnvcontaoformmanager/js/vue/library/vue.min.js' );
-$objAssetsManager->addIfNotExist( 'bundles/alnvcontaoformmanager/js/vue/library/vue-resource.min.js' );
+// $objAssetsManager = \Alnv\ContaoAssetsManagerBundle\Library\AssetsManager::getInstance();
+// $objAssetsManager->addIfNotExist( 'bundles/alnvcontaoformmanager/js/…' );
