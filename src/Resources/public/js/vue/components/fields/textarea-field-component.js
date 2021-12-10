4b0a941f127d9134fc92a90d358f2fcb4a28e104
Alexander Naumov <mail@alexandernaumov.de>
2021-12-10 15:42:36 +0100

    fix tiny mce

--- src/Resources/public/js/vue/components/fields/textarea-field-component.js
+++ src/Resources/public/js/vue/components/fields/textarea-field-component.js
@@ -21,22 +21,28 @@ Vue.component( 'textarea-field', {
             if (objTinyScriptTag) {
                 return null;
             }
-            let vue = this;
             let objScript = document.createElement('script');
-            objScript.id = 'tinyMCE';
-            objScript.onload = function () {
-                const strId = this.idPrefix + 'id_' + this.name;
-                tinymce.init({
-                    selector: '#' + strId,
-                    setup: function(editor) {
-                        editor.on('keyup', function () {
-                            vue.value = this.getContent();
-                        });
-                    }
-                });
-            }.bind(this);
-            objScript.src = 'bundles/alnvcontaoformmanager/js/libs/tinymce/tinymce.min.js';
+                objScript.id = 'tinyMCE';
+                objScript.src = 'bundles/alnvcontaoformmanager/js/libs/tinymce/tinymce.min.js';
             document.head.appendChild(objScript);
+        },
+        initTinymce: function () {
+            let vue = this;
+            const intRteWatch = setInterval(function () {
+                const objTinyScriptTag = document.getElementById('tinyMCE');
+                if (objTinyScriptTag) {
+                    clearInterval(intRteWatch);
+                    const strId = vue.idPrefix + 'id_' + vue.name;
+                    tinymce.init({
+                        selector: '#' + strId,
+                        setup: function(editor) {
+                            editor.on('keyup', function () {
+                                vue.value = this.getContent();
+                            });
+                        }
+                    });
+                }
+            }, 200);
         }
     },
     watch: {
@@ -69,13 +75,14 @@ Vue.component( 'textarea-field', {
     mounted: function() {
         if (this.eval.hasOwnProperty('rte') && this.eval['rte'] === 'tinyMCE') {
             this.loadTinymce();
+            this.initTinymce();
         }
     },
     template:
     '<div class="field-component textarea" v-bind:class="setCssClass()">' +
         '<div class="field-component-container">' +
             '<label class="label" :for="idPrefix + \'id_\' + name" v-html="eval.label"></label>' +
-            '<textarea v-if="eval.rte" :id="idPrefix + \'id_\' + name" v-html="value"></textarea>' +
+            '<textarea v-if="eval.rte" :id="idPrefix + \'id_\' + name" v-html="value" ref="textarea"></textarea>' +
             '<textarea v-if="!eval.rte" :id="idPrefix + \'id_\' + name" v-model="value"></textarea>' +
             '<template v-if="!eval.validate"><p class="error" v-for="message in eval.messages">{{ message }}</p></template>' +
             '<div v-if="eval.description" v-html="eval.description" class="info"></div>' +
